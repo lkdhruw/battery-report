fa5f65be97de3c12d47b6a797c2d6433195a3d47
Laxmi Kant Dhruw <lkdhruw@live.com>
2022-11-07 13:20:44 +0530

    Generated on 1320_2022-11-07

--- battery-report.bat
+++ battery-report.bat
@@ -2,9 +2,11 @@
 cd C:\Users\%userprofile%\Desktop
 powercfg /batteryreport
 
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-4 delims=/- " %%a in ('date /t') do (set mydate=%%c-%%b-%%a)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 
+echo %mytime%_%mydate%
+::pause
 git add .
 git commit -m "Generated on %mytime%_%mydate%"
 git push -u origin main
