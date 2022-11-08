f32ae03112622b61a6dd5bfc26e2a1fad12a9be5
Laxmi Kant Dhruw <lkdhruw@live.com>
2022-11-08 07:05:22 +0530

    Generated on 0705_2022-11-08

--- battery-report.bat
+++ battery-report.bat
@@ -1,7 +1,9 @@
 @ECHO OFF
 cd C:\Users\%userprofile%\Desktop
 powercfg /batteryreport
+if exist index.html del index.html
 ren battery-report.html index.html
+
 For /f "tokens=1-4 delims=/- " %%a in ('date /t') do (set mydate=%%c-%%b-%%a)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 
