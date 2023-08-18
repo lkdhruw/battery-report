3ec2e79c3562994adc57dcec342b5a1e4ed97fc9
Laxmi Kant Dhruw <lkdhruw@live.com>
2023-08-18 12:19:43 +0530

    Generated on 1219_2023-08-18

--- battery-report.bat
+++ battery-report.bat
@@ -8,7 +8,10 @@ For /f "tokens=1-4 delims=/- " %%a in ('date /t') do (set mydate=%%c-%%b-%%a)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 
 ::echo %mytime%_%mydate%
-echo. "%mytime%_%mydate%">%mydate%.txt
+::for %a in (index.html) do set FileDate=%~ta
+::FOR %A IN ("index.html") DO @ECHO=%~tA
+:: create new file
+::echo. %mytime%_%mydate%>%mydate%.txt
 pause
 git add .
 git commit -m "Generated on %mytime%_%mydate%"
